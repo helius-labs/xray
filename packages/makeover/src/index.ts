bd047e06402aece83b45fd6f98519027d9a6e822
ssamkkim <ssamuel.kkim@gmail.com>
2023-02-05 23:47:53 -0800

    fix: separate config for supported transactions

--- packages/makeover/src/index.ts
+++ packages/makeover/src/index.ts
@@ -1,5 +1,11 @@
 // // Format Helius transactions for display on the UI
-import type { EnrichedTransaction, Source } from "../../../../helius-sdk";
+import type { EnrichedTransaction } from "../../../../helius-sdk";
+
+import {
+    parseSwap,
+    parseTransfer,
+    parseUnknown
+} from "../parsers/parsers";
 
 // // TODO: Something here to make the data more like UI state
 
@@ -8,78 +14,20 @@ import type { EnrichedTransaction, Source } from "../../../../helius-sdk";
 //     // ...
 // }
 
-interface Transfer {
-    sendingUser: string | null,
-    receivingUser: string | null,
-    tokenTransferQuantity: number,
-    tokenTransferMintAddress: string,
-    source: Source;
-    timestamp: number;
-}
-
-const parseTransfer = (transaction: EnrichedTransaction): Transfer | EnrichedTransaction => {
-    if(transaction.tokenTransfers && transaction.tokenTransfers.length >= 1) {
-        const sendingUser = transaction.tokenTransfers[0].fromUserAccount;
-        const receivingUser = transaction.tokenTransfers[0].toUserAccount;
-        const tokenTransferQuantity = transaction.tokenTransfers[0].tokenAmount;
-        // const tokenTransferName = call a fetch to get the shorten named (ex: SOL, USDC, DeGods #1232)
-        const tokenTransferMintAddress = transaction.tokenTransfers[0].mint;
-        const { source, timestamp } = transaction;
-        // maybe translate timestamp in this v
-        
-        return {
-            sendingUser, receivingUser, tokenTransferQuantity, tokenTransferMintAddress, source, timestamp,
-        };
-    }
-    
-    return transaction;
+const supportedTransactions = {
+    TRANSFER : parseTransfer,
+    SWAP     : parseSwap,
+    UNKNOWN  : parseUnknown,
 };
 
-interface Swap {
-    swapUser: string | null;
-    tokenSwapped: string;
-    tokenSwappedAmount: number;
-    tokenReceived: string | undefined;
-    tokenReceivedAmount: number | undefined;
-    source: Source;
-    timestamp: number;
-}
-
-const parseSwap = (transaction: EnrichedTransaction): Swap | EnrichedTransaction => {
-    if(transaction.tokenTransfers && transaction.tokenTransfers.length >= 1) {
-        const swapUser = transaction.tokenTransfers[0].fromUserAccount;
-        const tokenSwapped = transaction.tokenTransfers[0].mint;
-        const tokenSwappedAmount = transaction.tokenTransfers[0].tokenAmount;
-        let tokenReceived;
-        let tokenReceivedAmount;
-        const { source, timestamp } = transaction;
-
-        for(let i = 1; i < transaction.tokenTransfers.length; i++) {
-            if(transaction.tokenTransfers[i].toUserAccount === swapUser) {
-                tokenReceived = transaction.tokenTransfers[i].mint;
-                tokenReceivedAmount = transaction.tokenTransfers[i].tokenAmount;
-            }
-        }
-
-        return {
-            swapUser, tokenSwapped, tokenSwappedAmount, tokenReceived, tokenReceivedAmount, source, timestamp,
-        };
-    }
-    
-    return transaction;
-};
-
-const parseUnknown = (transaction: EnrichedTransaction) => transaction;
+type SupportedTransactionTypes = keyof typeof supportedTransactions;
 
 export const parseTransaction = (transaction:EnrichedTransaction) => {
-    switch(transaction.type) {
-    case "TRANSFER":
-        return parseTransfer(transaction);
-    case "SWAP":
-        return parseSwap(transaction);
-    case "UNKNOWN":
-        return parseUnknown(transaction);
-    default:
+    const parser = supportedTransactions[transaction?.type as SupportedTransactionTypes];
+
+    if(!parser) {
         return parseUnknown(transaction);
     }
+
+    return parser(transaction);
 };
