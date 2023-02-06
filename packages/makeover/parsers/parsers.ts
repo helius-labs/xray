5e6c42a89e5cf41b81286cab25e0d84ea8f070e6
ssamkkim <ssamuel.kkim@gmail.com>
2023-02-05 23:48:46 -0800

    fix: separate files

---
+++ packages/makeover/parsers/parsers.ts
@@ -0,0 +1,9 @@
+import { parseSwap } from "./swap";
+import { parseTransfer } from "./transfer";
+import { parseUnknown } from "./unknown";
+
+export {
+    parseTransfer,
+    parseSwap,
+    parseUnknown,
+};
