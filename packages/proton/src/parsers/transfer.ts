de370326a26c7c32e069476318f3ce0a17897f68
qudo <qudo@qudos-Mac-Studio.local>
2023-02-06 23:35:06 -0800

    feat: native token transfers by scammo

--- packages/proton/src/parsers/transfer.ts
+++ packages/proton/src/parsers/transfer.ts
@@ -1,4 +1,5 @@
-import { EnrichedTransaction, Source } from "@helius/types";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import type { EnrichedTransaction, Source } from "@helius/types";
 
 interface Transfer {
     sendingUser: string | null,
@@ -11,12 +12,22 @@ interface Transfer {
 
 export const parseTransfer = (transaction: EnrichedTransaction): Transfer | EnrichedTransaction => {
     if(transaction.tokenTransfers) {
-        const [ firstTransaction ] = transaction.tokenTransfers;
+        let firstTransaction;
+        let tokenTransferQuantity;
+        let tokenTransferMintAddress;
+
+        if(transaction.tokenTransfers.length === 0 && transaction.nativeTransfers) {
+            [ firstTransaction ] = transaction.nativeTransfers;
+            tokenTransferQuantity = firstTransaction?.amount / LAMPORTS_PER_SOL;
+            tokenTransferMintAddress = "So11111111111111111111111111111111111111112";
+        } else {
+            [ firstTransaction ] = transaction.tokenTransfers;
+            tokenTransferQuantity = firstTransaction?.tokenAmount;
+            tokenTransferMintAddress = firstTransaction?.mint;
+        }
+        
         const sendingUser = firstTransaction?.fromUserAccount;
         const receivingUser = firstTransaction?.toUserAccount;
-        // const tokenTransferName = call a fetch to get the shorten named (ex: SOL, USDC, DeGods #1232)
-        const tokenTransferQuantity = firstTransaction?.tokenAmount;
-        const tokenTransferMintAddress = firstTransaction?.mint;
         const { source, timestamp } = transaction;
 
         return {
