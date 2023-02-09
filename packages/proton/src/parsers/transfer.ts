9c533035ef507439f1a8e60c9f8be303eb8a7596
ssamkkim <ssamuel.kkim@gmail.com>
2023-02-08 21:45:14 -0800

    feat: universal tx data shape, transfer support

--- packages/proton/src/parsers/transfer.ts
+++ packages/proton/src/parsers/transfer.ts
@@ -1,8 +1,12 @@
 import type { EnrichedTransaction } from "@helius/types";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
-
 import { ProtonTransaction, ProtonTransactionAction } from "./types";
 
+<<<<<<< HEAD
+import { ProtonTransaction, ProtonTransactionAction } from "./types";
+
+=======
+>>>>>>> 10cb258 (feat: universal tx data shape, transfer support)
 export const parseTransfer = (transaction: EnrichedTransaction): ProtonTransaction => {
     const { tokenTransfers, nativeTransfers } = transaction;
     const nativeTransfersLength = nativeTransfers.length - tokenTransfers.length;
@@ -10,6 +14,7 @@ export const parseTransfer = (transaction: EnrichedTransaction): ProtonTransacti
     const primaryUser = tokenTransfers.length ? tokenTransfers[0].fromUserAccount : nativeTransfers[0].fromUserAccount;
     const { type, source, timestamp } = transaction;
 
+<<<<<<< HEAD
     for(let i = 0; i < tokenTransfers.length; i++) {
         const [ tx ] = tokenTransfers;
         const from = tx.fromUserAccount;
@@ -48,4 +53,51 @@ export const parseTransfer = (transaction: EnrichedTransaction): ProtonTransacti
         actions,
     
     };
+=======
+    if(tokenTransfers) {
+        let firstTransaction;
+        const actions: ProtonTransactionAction[] = [];
+
+        if(tokenTransfers.length === 0 && nativeTransfers) {
+            firstTransaction = nativeTransfers[0];
+            const from = firstTransaction.fromUserAccount;
+            const to = firstTransaction.toUserAccount;
+            const sent = "So11111111111111111111111111111111111111112";
+            const amount = firstTransaction.amount / LAMPORTS_PER_SOL;
+
+            actions.push({
+                from,
+                sent,
+                to,
+                amount,
+            });
+        } else {
+            firstTransaction = tokenTransfers[0];
+            const from = firstTransaction.fromUserAccount;
+            const to = firstTransaction.toUserAccount;
+            const sent = firstTransaction.mint;
+            const amount = firstTransaction.tokenAmount;
+
+            actions.push({
+                from,
+                sent,
+                to,
+                amount,
+            });
+        }
+        
+        const primaryUser = firstTransaction?.fromUserAccount;
+        const { type, source, timestamp } = transaction;
+
+        return {
+            type,
+            primaryUser,
+            timestamp,
+            source,
+            actions,
+        };
+    }
+    
+    return transaction;
+>>>>>>> 10cb258 (feat: universal tx data shape, transfer support)
 };
