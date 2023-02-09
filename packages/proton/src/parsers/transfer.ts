ca842f4f18d6c701b963e5b428c8793f3088b7a9
ssamkkim <ssamuel.kkim@gmail.com>
2023-02-08 22:07:29 -0800

    feat: bulk send support for transfer parsing

--- packages/proton/src/parsers/transfer.ts
+++ packages/proton/src/parsers/transfer.ts
@@ -5,49 +5,49 @@ import { ProtonTransaction, ProtonTransactionAction } from "./types";
 export const parseTransfer = (transaction: EnrichedTransaction): ProtonTransaction => {
     const { tokenTransfers, nativeTransfers } = transaction;
 
-    if(tokenTransfers) {
-        let firstTransaction;
-        const actions: ProtonTransactionAction[] = [];
+    const nativeTransfersLength = nativeTransfers.length - tokenTransfers.length;
 
-        if(tokenTransfers.length === 0 && nativeTransfers) {
-            firstTransaction = nativeTransfers[0];
-            const from = firstTransaction.fromUserAccount;
-            const to = firstTransaction.toUserAccount;
-            const sent = "So11111111111111111111111111111111111111112";
-            const amount = firstTransaction.amount / LAMPORTS_PER_SOL;
+    const actions: ProtonTransactionAction[] = [];
 
-            actions.push({
-                from,
-                sent,
-                to,
-                amount,
-            });
-        } else {
-            firstTransaction = tokenTransfers[0];
-            const from = firstTransaction.fromUserAccount;
-            const to = firstTransaction.toUserAccount;
-            const sent = firstTransaction.mint;
-            const amount = firstTransaction.tokenAmount;
+    for(let i = 0; i < tokenTransfers.length; i++) {
+        const [ tx ] = tokenTransfers;
+        const from = tx.fromUserAccount;
+        const to = tx.toUserAccount;
+        const sent = tx.mint;
+        const amount = tx.tokenAmount;
+    
+        actions.push({
+            from,
+            sent,
+            to,
+            amount,
+        });
+    }
 
-            actions.push({
-                from,
-                sent,
-                to,
-                amount,
-            });
-        }
-        
-        const primaryUser = firstTransaction?.fromUserAccount;
-        const { type, source, timestamp } = transaction;
+    for(let i = 0; i < nativeTransfersLength; i++) {
+        const [ tx ] = nativeTransfers;
+        const from = tx.fromUserAccount;
+        const to = tx.toUserAccount;
+        const sent = "So11111111111111111111111111111111111111112";
+        const amount = tx.amount / LAMPORTS_PER_SOL;
 
-        return {
-            type,
-            primaryUser,
-            timestamp,
-            source,
-            actions,
-        };
+        actions.push({
+            from,
+            sent,
+            to,
+            amount,
+        });
     }
     
-    return transaction;
+    const primaryUser = tokenTransfers.length ? tokenTransfers[0].fromUserAccount : nativeTransfers[0].fromUserAccount;
+    const { type, source, timestamp } = transaction;
+
+    return {
+        type,
+        primaryUser,
+        timestamp,
+        source,
+        actions,
+    
+    };
 };
