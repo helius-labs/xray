6dd94c850408db0f2f44054fd01a091433c725e6
ssamkkim <ssamuel.kkim@gmail.com>
2023-02-08 22:27:41 -0800

    feat: ProtonTransaction support for swap tx type

--- packages/proton/src/parsers/swap.ts
+++ packages/proton/src/parsers/swap.ts
@@ -10,8 +10,12 @@ export const parseSwap = (transaction: EnrichedTransaction): ProtonTransaction =
 
     for(let i = 0; i < tokenTransfers.length; i++) {
         const [ tx ] = tokenTransfers;
+<<<<<<< HEAD
         let sent;
         let received;
+=======
+        let sent; let received;
+>>>>>>> 9c4a140 (feat: ProtonTransaction support for swap tx type)
 
         if(tx.fromUserAccount === primaryUser) {
             sent = tx.mint;
@@ -26,8 +30,13 @@ export const parseSwap = (transaction: EnrichedTransaction): ProtonTransaction =
         if(sent) {
             actions.push({
                 from,
+<<<<<<< HEAD
                 to,
                 sent,
+=======
+                sent,
+                to,
+>>>>>>> 9c4a140 (feat: ProtonTransaction support for swap tx type)
                 amount,
             });
         } else {
--- packages/proton/src/parsers/transfer.ts
+++ packages/proton/src/parsers/transfer.ts
@@ -1,5 +1,6 @@
 import type { EnrichedTransaction } from "@helius/types";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
 import { ProtonTransaction, ProtonTransactionAction } from "./types";
 
 <<<<<<< HEAD
@@ -9,6 +10,7 @@ import { ProtonTransaction, ProtonTransactionAction } from "./types";
 >>>>>>> 10cb258 (feat: universal tx data shape, transfer support)
 export const parseTransfer = (transaction: EnrichedTransaction): ProtonTransaction => {
     const { tokenTransfers, nativeTransfers } = transaction;
+<<<<<<< HEAD
     const nativeTransfersLength = nativeTransfers.length - tokenTransfers.length;
     const actions: ProtonTransactionAction[] = [];
     const primaryUser = tokenTransfers.length ? tokenTransfers[0].fromUserAccount : nativeTransfers[0].fromUserAccount;
@@ -62,7 +64,12 @@ export const parseTransfer = (transaction: EnrichedTransaction): ProtonTransacti
     const nativeTransfersLength = nativeTransfers.length - tokenTransfers.length;
 >>>>>>> ca842f4 (feat: bulk send support for transfer parsing)
 
+=======
+    const nativeTransfersLength = nativeTransfers.length - tokenTransfers.length;
+>>>>>>> 9c4a140 (feat: ProtonTransaction support for swap tx type)
     const actions: ProtonTransactionAction[] = [];
+    const primaryUser = tokenTransfers.length ? tokenTransfers[0].fromUserAccount : nativeTransfers[0].fromUserAccount;
+    const { type, source, timestamp } = transaction;
 
     for(let i = 0; i < tokenTransfers.length; i++) {
         const [ tx ] = tokenTransfers;
@@ -93,6 +100,7 @@ export const parseTransfer = (transaction: EnrichedTransaction): ProtonTransacti
             amount,
         });
     }
+<<<<<<< HEAD
     
 <<<<<<< HEAD
     return transaction;
@@ -100,6 +108,8 @@ export const parseTransfer = (transaction: EnrichedTransaction): ProtonTransacti
 =======
     const primaryUser = tokenTransfers.length ? tokenTransfers[0].fromUserAccount : nativeTransfers[0].fromUserAccount;
     const { type, source, timestamp } = transaction;
+=======
+>>>>>>> 9c4a140 (feat: ProtonTransaction support for swap tx type)
 
     return {
         type,
