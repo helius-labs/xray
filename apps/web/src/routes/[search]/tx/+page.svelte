fedc56b34022d442de2fbb3a3456bfa58ef25b24
qudo <qudo@qudos-Mac-Studio.local>
2023-02-10 01:08:33 -0800

    feat: start of tx details design

--- apps/web/src/routes/[search]/tx/+page.svelte
+++ apps/web/src/routes/[search]/tx/+page.svelte
@@ -10,15 +10,16 @@
     import TxSendReceive from "$lib/components/transaction/send-receive.svelte";
     import TxSwap from "$lib/components/transaction/swap.svelte";
     import TxFooter from "$lib/components/transaction/footer.svelte";
+    import shortenString from "src/lib/util/shorten-string";
+    import Icon from "src/lib/icon/icon.svelte";
   
     const tokenRegistry = query("solana-token-registry");
 
     const transaction = query("solana-transaction");
 
     $: if($transaction?.load && $page.params.search) {
-            $transaction.load($page.params.search);
+        $transaction.load($page.params.search);
     }
-
 </script>
 
 {#if $transaction?.isLoading}
@@ -26,6 +27,104 @@
 {:else if $transaction?.isError}
     <p>Error: {$transaction?.error}</p>
 {:else if $transaction?.hasFetched}
+    <div class="card prose mb-6">
+        <div class="flex justify-between items-center">
+            <div>
+                <h4 class="font-bold opacity-50 m-0">Transaction</h4>
+                <div class="flex items-center">
+                    <h3 class="m-0">{shortenString($transaction?.data?.parsed?.signature || "")}</h3>
+                    <button class="btn btn-ghost">
+                        <Icon id="copy" />
+                    </button>
+                </div>
+            </div>
+            <div>
+                <div class="badge badge-success h-8 px-3">
+                    Success
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {#each $transaction?.data?.parsed?.actions as {
+        from,
+        to,
+        amount,
+        sent,
+    }}
+        <div class="grid grid-cols-4 gap-4">
+            <div class="col-span-3">
+                <div class="grid grid-cols-12 gap-4">
+                    <div class="col-span-4">
+                        <div class="card">
+                            <div class="center">
+                                <Icon id="paper-plane" />
+                                <p class="m-0  ml-2 font-bold">Sent</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-8">
+                        <div class="card">
+                            <div class="flex items-center justify-between">
+                                <p class="m-0  ml-2 font-bold opacity-50">
+                                    {#if $tokenRegistry.data.has(sent)}
+                                        {$tokenRegistry.data.get(sent).symbol}
+                                    {:else}
+                                        UNKNOWN
+                                    {/if}
+                                </p>
+                                
+                                <p class="m-0  ml-2 font-bold">{Number(amount).toFixed(8)}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-5">
+                        <div class="card">
+                            <div class="flex items-center">
+                                <Icon
+                                    id="person"
+                                    size="md" />
+                                <div>
+                                    <p class="m-0  ml-2 font-bold opacity-50 text-xs">From</p>
+                                    <p class="m-0  ml-2 font-bold">{shortenString(from)}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-2 center">
+                        <Icon
+                            id="arrow-right"
+                            size="lg" />
+                    </div>
+                    <div class="col-span-5">
+                        <div class="card">
+                            <div class="flex items-center">
+                                <Icon
+                                    id="person"
+                                    size="md" />
+                                <div>
+                                    <p class="m-0  ml-2 font-bold opacity-50 text-xs">To</p>
+                                    <p class="m-0  ml-2 font-bold">{shortenString(to)}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-span-1">
+                <div class="card h-full center">
+                    {#if $tokenRegistry.data.has(sent)}
+                        <img
+                            class="w-3/4"
+                            alt=""
+                            src={$tokenRegistry.data.get(sent).logoURI}>
+                    {:else}
+                        UNKNOWN
+                    {/if}
+                </div>
+            </div>
+        </div>
+    {/each}
     <!-- <div class="text-xs card mb-3">
         Parsed
         <pre>{JSON.stringify($transaction?.data?.parsed, null, 4)}</pre>
@@ -34,7 +133,7 @@
         Raw
         <pre>{JSON.stringify($transaction?.data?.raw, null, 4)}</pre>
     </div> -->
-    <div class="grid grid-col-1 mx-auto">
+    <!-- <div class="grid grid-col-1 mx-auto">
 
         <TxType />
         {#if $transaction?.data?.parsed.type === "TRANSFER"}
@@ -50,6 +149,6 @@
             <TxFooter />
         {/if}
         <TxFooter />
-    </div>
+    </div> -->
 {/if}
 
