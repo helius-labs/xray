aa70f8dd12d3661106e3ef1a88c3fa19d78e9108
ssamkkim <ssamuel.kkim@gmail.com>
2023-02-14 17:15:02 -0800

    feat: (temp) sol domain search resolve endpoint

--- apps/web/src/routes/[search]/+layout.server.ts
+++ apps/web/src/routes/[search]/+layout.server.ts
@@ -1,9 +1,11 @@
 /* eslint-disable max-statements */
 import type { RequestEvent } from "./$types";
 
-import type {
-    ParsedAccountData
-} from "@solana/web3.js";
+import {
+    getDomainKey,
+    NameRegistryState
+} from "@bonfida/spl-name-service";
+import type { ParsedAccountData } from "@solana/web3.js";
 
 import { redirect } from "@sveltejs/kit";
 
@@ -27,6 +29,7 @@ export async function load({ params, url }: RequestEvent) {
     // TODO: better way to check if it's a tx?
     // They will be presented with an error on the tx page if it's not.
     const probablyTransactionSignature = params.search.length > 50;
+    const probablySolanaName = params.search.slice(-4) === ".sol";
 
     const pubKey = validatePubkey(params.search);
 
@@ -42,6 +45,23 @@ export async function load({ params, url }: RequestEvent) {
         const redirectUrl = program === "spl-token" ? `/${params.search}/token` : `/${params.search}/wallet`;
 
         throw redirect(307, redirectUrl);
+    } else if(probablySolanaName) {
+        try {
+            // const { pubkey } = await getDomainKey(params.search);
+            // const data = await NameRegistryState.retrieve(
+            //     connection,
+            //     pubkey,
+            // );
+            const address = await fetch(`https://resolve-solana-domain.qudo-code.repl.co/domain/${params.search}`)
+                .then((res) => res.json());
+            const redirectUrl = address ? `/${address}/wallet` : `/${params.search}/?error="invalid-search"`;
+            // const redirectUrl = data.registry.owner ? `/${data.registry.owner}/wallet` : `/${params.search}/?error="invalid-search"`;
+
+            throw redirect(307, redirectUrl);
+        } catch(error) {
+            // console.log(error);
+            throw redirect(307, `/${params.search}/?error="invalid-search"`);
+        }
     } else {
         throw redirect(307, `/${params.search}/?error="invalid-search"`);
     }
--- apps/web/src/routes/api/search/[search]/+server.ts
+++ apps/web/src/routes/api/search/[search]/+server.ts
@@ -1,13 +1,17 @@
-import { json, type RequestEvent } from "@sveltejs/kit";
+import {
+    json,
+    type RequestEvent
+} from "@sveltejs/kit";
 
-import type {
-    ParsedAccountData
-} from "@solana/web3.js";
+import {
+    getDomainKey,
+    NameRegistryState
+} from "@bonfida/spl-name-service";
+import type { ParsedAccountData } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 
 import connect from "$lib/util/solana/connect";
 import validPublicKey from "$lib/util/solana/validate-pubkey";
-
-import { PublicKey } from "@solana/web3.js";
 
 // Consume a search, return what to do with it
 export async function GET({ params }: RequestEvent) {
@@ -17,6 +21,7 @@ export async function GET({ params }: RequestEvent) {
     // If it's long, assume it's a tx.
     // They will be presented with an error on the tx page if it's not.
     const probablyTransactionSignature = search.length > 50;
+    const probablySolanaName = search.slice(-4) === ".sol";
     
     if(validPublicKey(search)) {
         const pubkey = new PublicKey(search);
@@ -31,23 +36,52 @@ export async function GET({ params }: RequestEvent) {
          
         return json({
             data : {
-                valid : true,
                 url,
+                valid : true,
             },
         });
     } else if(probablyTransactionSignature) {
         return json({
             data : {
-                valid : true,
                 url   : `/${params?.search}/tx`,
+                valid : true,
             },
         });
+    } else if(probablySolanaName) {
+        try {
+            // const { pubkey } = await getDomainKey(search);
+            // const data = await NameRegistryState.retrieve(
+            //     connection,
+            //     pubkey,
+            // );
+
+            // const url = data.registry.owner ? `/${data.registry.owner}/wallet` : `/${params.search}/?error="invalid-search"`;
+            const address = await fetch(`https://resolve-solana-domain.qudo-code.repl.co/domain/${search}`)
+                .then((res) => res.json());
+            const url = address ? `/${address}/wallet` : `${params.search}/?error="invalid-search"`;
+            
+            return json({
+                data : {
+                    url,
+                    valid : true,
+                },
+            });
+        } catch(error) {
+            // console.log(error);
+            
+            return json({
+                data : {
+                    url   : `/`,
+                    valid : false,
+                },
+            });
+        }
     }
  
     return json({
         data : {
-            valid : false,
             url   : `/`,
+            valid : false,
         },
     });
 }
