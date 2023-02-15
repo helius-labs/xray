ecd4c9aa2a35ce092cd80ec9f3b23b7266754dcd
qudo <qudo@qudos-Mac-Studio.local>
2023-02-14 17:02:19 -0800

    vercel: build

--- apps/web/src/lib/util/solana/connect.ts
+++ apps/web/src/lib/util/solana/connect.ts
@@ -1,10 +1,10 @@
 import { Connection } from "@solana/web3.js";
 
-import { HELIUS_KEY } from "$env/static/private";
+const { HELIUS_KEY } = process.env;
 
 const networks = {
-    mainnet: `https://rpc.helius.xyz/?api-key=${HELIUS_KEY}`,
     devnet: `https://rpc-devnet.helius.xyz/?api-key=${HELIUS_KEY}`,
+    mainnet: `https://rpc.helius.xyz/?api-key=${HELIUS_KEY}`,
 };
 
 export default (network: "devnet" | "mainnet" = "mainnet") =>
