75750ef272ee1c45a34e17496b3fdc3d4bd4362a
qudo <qudo@qudos-Mac-Studio.local>
2023-02-14 21:48:32 -0800

    fix: vercel config

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
     kit: {
-        adapter: adapter({ runtime: "edge" }),
+        adapter: adapter(),
         alias: {
             "@helius-labs/*": "../../packages/*",
             src: "./src",
