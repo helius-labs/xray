c317f6b593b716ccb14a189c35002548c193aa71
qudo <59782726+qudo-code@users.noreply.github.com>
2023-02-19 01:54:43 -0800

    chore: cleanup (#46)
    Co-authored-by: qudo <qudo@qudos-Mac-Studio.local>

--- apps/web/src/lib/components/footer.svelte
+++ apps/web/src/lib/components/footer.svelte
@@ -6,11 +6,11 @@
 <footer
     class="footer fixed bottom-0 grid w-full grid-cols-2 items-center border bg-black p-3 pb-5"
 >
-    <div class="flex items-center">
+    <div class="col-span-1 flex items-center">
         <PoweredByHelius />
     </div>
 
-    <div class="flex w-full justify-end">
+    <div class="col-span-1 flex w-full justify-end">
         <div>
             <a
                 href="https://discord.gg/Wkn3uuSby7"
--- apps/web/src/lib/components/providers/token-provider.svelte
+++ apps/web/src/lib/components/providers/token-provider.svelte
@@ -3,14 +3,14 @@
     import type { UITokenMetadata } from "$lib/types";
     import { state, type QueryStore } from "svelte-snacks";
 
-    export let search:string;
-    
+    export let search: string = "";
+
     const solanaToken: QueryStore = state(["solanaToken", search], search);
-    
+
     const tokenRegistry = state("solanaTokenRegistry");
 
     const metadata: UITokenMetadata = {
-        address : "",
+        address: "",
         attributes: [],
         collectionKey: "",
         creators: [],
@@ -32,18 +32,25 @@
         } else {
             metadata.address = $solanaToken.data?.account;
             metadata.name = $solanaToken.data?.offChainMetadata?.metadata?.name;
-            metadata.image = $solanaToken.data?.offChainMetadata?.metadata?.image;
-            metadata.description = $solanaToken.data?.offChainMetadata?.metadata?.description;
-            metadata.attributes = $solanaToken.data?.offChainMetadata?.metadata?.attributes;
-            metadata.creators = $solanaToken.data?.onChainMetadata?.metadata?.data?.creators;
-            metadata.collectionKey = $solanaToken.data?.onChainMetadata?.metadata?.collection?.key;
+            metadata.image =
+                $solanaToken.data?.offChainMetadata?.metadata?.image;
+            metadata.description =
+                $solanaToken.data?.offChainMetadata?.metadata?.description;
+            metadata.attributes =
+                $solanaToken.data?.offChainMetadata?.metadata?.attributes;
+            metadata.creators =
+                $solanaToken.data?.onChainMetadata?.metadata?.data?.creators;
+            metadata.collectionKey =
+                $solanaToken.data?.onChainMetadata?.metadata?.collection?.key;
         }
-    };
-
+    }
 
     onMount(() => {
         isReady = true;
     });
 </script>
 
-<slot {metadata} token={$solanaToken} />
+<slot
+    {metadata}
+    token={$solanaToken}
+/>
