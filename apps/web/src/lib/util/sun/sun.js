ba19612640c0d0997211b7eedeb44264723c9153
Miles Smith <miles@helius.xyz>
2023-03-08 23:37:43 -0500

    fix: increase flare sizes

--- apps/web/src/lib/util/sun/sun.js
+++ apps/web/src/lib/util/sun/sun.js
@@ -81,7 +81,7 @@ export const createScene = (el, assetsLoadedCb) => {
   
       void main() {
         vUv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 1.03, 1.0);
       }
     `,
         fragmentShader: `
