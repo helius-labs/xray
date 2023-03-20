217fbd9d53bef253b7e1d65c47e942829dc2ea51
ssamkkim <ssamuel.kkim@gmail.com>
2023-03-20 03:48:20 -0700

    recent slot route

---
+++ apps/web/src/lib/trpc/routes/current-slot.ts
@@ -0,0 +1,11 @@
+import { t } from "$lib/trpc/t";
+
+import connect from "src/lib/util/solana/connect";
+
+export const currentSlot = t.procedure.query(async () => {
+    const connection = connect();
+
+    const slot = await connection.getSlot();
+
+    return slot;
+});
