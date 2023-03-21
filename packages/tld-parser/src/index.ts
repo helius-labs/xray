9aa377a1e831ae4727957f8e8cf52031b6a9f36f
Vlad <tonivlad@yahoo.com>
2023-03-21 13:09:42 +0200

    updated @onsol/tldparser to 0.4.5 and deleted tld-parser from packages

--- packages/tld-parser/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './utils';
-export * from './constants';
-export * from './parsers';
-export * from './state/name-record-header';
-export * from './name-record-handler';
-export * from './types/records';
