3841c2e5c158fd48a551269fe423ddeb59ed810f
ssamkkim <ssamuel.kkim@gmail.com>
2023-03-22 22:46:47 -0700

    header adjustments

--- apps/web/src/lib/components/account-header.svelte
+++ apps/web/src/lib/components/account-header.svelte
@@ -8,6 +8,7 @@
     import { onMount } from "svelte";
     import { tweened } from "svelte/motion";
     import { trpcWithQuery } from "../trpc/client";
+    import Namor from "./providers/namor-provider.svelte";
 
     const client = trpcWithQuery($page);
 
@@ -34,39 +35,44 @@
     $: worth = $balance * $price?.data;
 </script>
 
-<div class="nav sticky top-16 z-30 bg-base-100 px-3 pt-2">
-    <div class="flex flex-wrap items-center justify-between bg-base-100">
-        <div>
-            <h3
-                class="tooltip tooltip-bottom tooltip-secondary relative m-0 text-lg font-bold md:text-2xl"
-                data-tip="A nickname generated for this account"
-            >
-                {shortenString(account)}
-            </h3>
-            <div class="relative flex items-center">
-                <p class="mr-3 text-xs opacity-50">
-                    {shortenString(account)}
-                </p>
-                <div class="my-2">
-                    <CopyButton text={account} />
-                    <CopyButton
-                        text={link}
-                        icon="link"
-                    />
+<Namor
+    text={account}
+    let:result
+>
+    <div class="nav sticky top-16 z-30 bg-base-100 px-3 pt-2">
+        <div class="flex flex-wrap items-center justify-between bg-base-100">
+            <div>
+                <h3
+                    class="tooltip tooltip-bottom tooltip-secondary relative m-0 text-lg font-bold md:text-2xl"
+                    data-tip="A nickname generated for this account"
+                >
+                    {result.substring(0, result.indexOf(" "))}
+                </h3>
+                <div class="relative flex items-center">
+                    <p class="mr-3 text-xs opacity-50">
+                        {shortenString(account)}
+                    </p>
+                    <div class="my-2">
+                        <CopyButton text={account} />
+                        <CopyButton
+                            text={link}
+                            icon="link"
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="relative text-right">
-            <h1 class="text-md hidden md:block">
-                <span class="">{$balance.toFixed(6)}</span>
-                <span class="opacity-50">SOL</span>
-            </h1>
+            <div class="relative text-right">
+                <h1 class="text-md md:block">
+                    <span class="">{$balance.toFixed(6)}</span>
+                    <span class="opacity-50">SOL</span>
+                </h1>
 
-            {#if !$price?.isLoading}
-                <span class="ml-1 hidden text-xs opacity-50 md:block"
-                    >{formatMoney(worth)} USD</span
-                >
-            {/if}
+                {#if !$price?.isLoading}
+                    <span class="ml-1 text-xs opacity-50 md:block"
+                        >{formatMoney(worth)} USD</span
+                    >
+                {/if}
+            </div>
         </div>
     </div>
-</div>
+</Namor>
--- apps/web/src/routes/[search]/wallet/+layout.svelte
+++ apps/web/src/routes/[search]/wallet/+layout.svelte
@@ -30,40 +30,11 @@
 
     const account = $page.params.search;
 
-    const client = trpcWithQuery($page);
-
-    const accountInfo = client.accountInfo.createQuery(account);
-    const price = client.price.createQuery(SOL);
-
-    const balance = tweened(0, {
-        duration: 1000,
-    });
-
-    let animate = false;
     let filter = "";
-
-    $: if ($accountInfo?.data?.balance) {
-        balance.set($accountInfo.data.balance);
-    }
-
-    onMount(() => {
-        animate = true;
-    });
-
-    $: worth = $balance * $price?.data;
 </script>
 
-<div class="flex items-center px-3 md:hidden">
-    <h1 class="my-1 text-lg">
-        <span class="">{$balance.toFixed(6)}</span>
-        <span class="opacity-50">SOL</span>
-    </h1>
-    <span class="ml-3 text-xs opacity-50 md:block"
-        >{formatMoney(worth)} USD</span
-    >
-</div>
 <AccountHeader
-    account={$page.params.search}
+    {account}
     link={$page.url.href}
 />
 <div>
