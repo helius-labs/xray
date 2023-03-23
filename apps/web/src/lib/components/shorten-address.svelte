7310d7b011c570f9d092538c696bc032e0d023db
ssamkkim <ssamuel.kkim@gmail.com>
2023-03-22 23:25:38 -0700

    shorten address component

---
+++ apps/web/src/lib/components/shorten-address.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let address: string = "";
+</script>
+
+<h3 class="text-md">
+    <span class="font-bold">{address.substring(0, 4)}</span><span
+        class="opacity-50">...{address.slice(-4)}</span
+    >
+</h3>
--- apps/web/src/lib/util/search.ts
+++ apps/web/src/lib/util/search.ts
@@ -8,11 +8,11 @@ import { getDomainKey, NameRegistryState } from "@bonfida/spl-name-service";
 
 import { PublicKey } from "@solana/web3.js";
 
-import { TldParser } from "@onsol/tldparser";
+// import { TldParser } from "@onsol/tldparser";
 
 export default async (search: string) => {
     const connection = connect();
-    const ans = new TldParser(connection);
+    // const ans = new TldParser(connection);
 
     // If it's long, assume it's a tx.
     // They will present with an error on the tx page if it's not.
@@ -111,25 +111,26 @@ export default async (search: string) => {
         };
 
         return { data };
-    } else if (isDomain) {
-        const owner = await ans.getOwnerFromDomainTld(search);
-
-        if (!owner) {
-            return {
-                data: {
-                    url: `/`,
-                    valid: false,
-                },
-            };
-        }
-
-        return {
-            data: {
-                url: `/${owner}/wallet`,
-                valid: true,
-            },
-        };
     }
+    // else if (isDomain) {
+    //     const owner = await ans.getOwnerFromDomainTld(search);
+
+    //     if (!owner) {
+    //         return {
+    //             data: {
+    //                 url: `/`,
+    //                 valid: false,
+    //             },
+    //         };
+    //     }
+
+    //     return {
+    //         data: {
+    //             url: `/${owner}/wallet`,
+    //             valid: true,
+    //         },
+    //     };
+    // }
 
     return {
         data: {
