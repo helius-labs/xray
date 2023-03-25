c85a0bec9f63e827769af600cc0202b08eee08c8
ssamkkim <ssamuel.kkim@gmail.com>
2023-03-25 05:51:36 -0700

    fixed duplicated current slot

--- apps/web/src/lib/components/stats.svelte
+++ apps/web/src/lib/components/stats.svelte
@@ -66,24 +66,4 @@
             <div class="pulse my-2 h-2 w-32 rounded-lg bg-secondary" />
         {/if}
     </div>
-    <div>
-        {#if !$tps.isLoading}
-            <div
-                in:fade={{
-                    duration: 500,
-                }}
-            />
-            <span class="font-bold">Current Slot </span>
-            <span class="ml-1 opacity-50 hover:opacity-100">
-                <a
-                    data-sveltekit-reload
-                    href="/block/{$slot?.data}/"
-                    class="pointer-events-auto hover:link-success"
-                    >{$slot?.data?.toLocaleString()}</a
-                >
-            </span>
-        {:else}
-            <div class="pulse my-2 h-2 w-32 rounded-lg bg-secondary" />
-        {/if}
-    </div>
 </div>
