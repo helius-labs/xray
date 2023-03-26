fe90809cc8561ec2da4f717da6e1cbe9108e2cfc
ssamkkim <ssamuel.kkim@gmail.com>
2023-03-26 00:09:19 -0700

    simplified username functions & get multiple .sol

--- apps/web/src/lib/trpc/routes/account-usernames.ts
+++ apps/web/src/lib/trpc/routes/account-usernames.ts
@@ -3,46 +3,65 @@ import { t } from "$lib/trpc/t";
 import { z } from "zod";
 
 import { getAllDomains, reverseLookup } from "@bonfida/spl-name-service";
-import { TldParser } from "@onsol/tldparser";
+// import { TldParser } from "@onsol/tldparser";
 import { Connection, PublicKey } from "@solana/web3.js";
 import connect from "src/lib/util/solana/connect";
 
-const getBackpackUsername = async (address = "") => {
+interface Username {
+    type: "bonfida" | "backpack" | "ans";
+    username: string;
+}
+
+const getBackpackUsername = async (usernames: Username[], address = "") => {
     const response = await fetch(
         `https://xnft-api-server.xnfts.dev/v1/users/fromPubkey?publicKey=${address}&blockchain=solana`
     );
     const data = await response.json();
 
-    return data?.user?.username || "";
+    if (data?.user?.username) {
+        usernames.push({
+            type: "backpack",
+            username: data.user.username,
+        });
+    }
 };
 
-const getSolanaDomain = async (address = "", connection: Connection) => {
+const getSolanaDomain = async (
+    usernames: Username[],
+    address = "",
+    connection: Connection
+) => {
     const domainKey = new PublicKey(address);
     const allDomainKeys = await getAllDomains(connection, domainKey);
+    const domains: string[] = [];
     if (allDomainKeys) {
-        const domainName = await reverseLookup(connection, allDomainKeys[0]);
-        return domainName;
+        for (const key of allDomainKeys) {
+            const domainName = await reverseLookup(connection, key);
+            usernames.push({
+                type: "bonfida",
+                username: `${domainName}.sol`,
+            });
+        }
     }
-
-    return "";
+    return domains;
 };
 
-const getANSDomain = async (address = "", connection: Connection) => {
-    const ans = new TldParser(connection);
-    let domain;
+// const getANSDomain = async (address = "", connection: Connection) => {
+//     const ans = new TldParser(connection);
+//     let domain;
 
-    try {
-        domain = await ans.getMainDomain(address);
-    } catch (error) {
-        return "";
-    }
+//     try {
+//         domain = await ans.getMainDomain(address);
+//     } catch (error) {
+//         return "";
+//     }
 
-    if (domain && domain?.domain && domain?.tld) {
-        return `${domain.domain}${domain.tld}`;
-    }
+//     if (domain && domain?.domain && domain?.tld) {
+//         return `${domain.domain}${domain.tld}`;
+//     }
 
-    return "";
-};
+//     return "";
+// };
 
 export const accountUsernames = t.procedure
     .input(z.string())
@@ -56,28 +75,16 @@ export const accountUsernames = t.procedure
     )
     .query(async ({ input: address }) => {
         const connection = connect();
-        const usernames = [];
-        const backpackUsername = await getBackpackUsername(address);
-        const solanaDomain = await getSolanaDomain(address, connection);
-        const ansDomain = await getANSDomain(address, connection);
+        const usernames: Username[] = [];
+        await getBackpackUsername(usernames, address);
+        await getSolanaDomain(usernames, address, connection);
+        // const ansDomain = await getANSDomain(address, connection);
 
-        if (backpackUsername) {
-            usernames.push({
-                type: "backpack",
-                username: backpackUsername,
-            });
-        }
-        if (solanaDomain) {
-            usernames.push({
-                type: "bonfida",
-                username: `${solanaDomain}.sol`,
-            });
-        }
-        if (ansDomain) {
-            usernames.push({
-                type: "ans",
-                username: ansDomain,
-            });
-        }
+        // if (ansDomain) {
+        //     usernames.push({
+        //         type: "ans",
+        //         username: ansDomain,
+        //     });
+        // }
         return usernames || [];
     });
