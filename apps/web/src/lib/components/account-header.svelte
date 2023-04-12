89f7932df2d2274a465db69d3d3766f651ae4b6a
ssamkkim <ssamuel.kkim@gmail.com>
2023-04-11 23:12:36 -0700

    temp domain

--- apps/web/src/lib/components/account-header.svelte
+++ apps/web/src/lib/components/account-header.svelte
@@ -54,45 +54,73 @@
     $: worth = $balance * $price?.data;
 </script>
 
-<div class="nav sticky top-16 z-30 bg-base-100 px-3 pt-2">
-    <div class="flex flex-wrap items-center justify-between bg-base-100">
-        <div class="flex items-center">
-            <h3 class="relative m-0 text-lg font-bold md:text-2xl">
-                <ShortenAddress address={account} />
-            </h3>
-            <div class="relative flex items-center">
-                <div class="my-2">
-                    <CopyButton text={account} />
-                    <CopyButton
-                        text={link}
-                        icon="link"
-                    />
+<Username
+    address={account}
+    let:usernames
+    let:usernameIsLoading
+>
+    <div class="nav sticky top-16 z-30 bg-base-100 px-3 pt-2">
+        <div class="flex flex-wrap items-center justify-between bg-base-100">
+            <div class="flex flex-col">
+                <div class="flex items-center">
+                    <h3 class="relative m-0 text-lg font-bold md:text-2xl">
+                        <ShortenAddress address={account} />
+                    </h3>
+                    <div class="relative flex items-center">
+                        <div class="my-2">
+                            <CopyButton text={account} />
+                            <CopyButton
+                                text={link}
+                                icon="link"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-wrap gap-2">
+                    {#if usernameIsLoading}
+                        {#each [1, 2, 3] as _}
+                            <div
+                                class="username-block inline-block h-6 w-[72px] animate-pulse rounded-full py-1 px-3 text-xs font-extrabold"
+                            />
+                        {/each}
+                    {:else if usernames && usernames?.length > 0}
+                        {#each usernames as username}
+                            {#if username.type === "backpack"}
+                                <div
+                                    class="inline-block rounded-full bg-red-200 py-1 px-3 text-xs font-extrabold text-red-600"
+                                >
+                                    <div class="flex items-center gap-1">
+                                        <Icon
+                                            id="backpack"
+                                            size="sm"
+                                        />
+
+                                        {username.username}
+                                    </div>
+                                </div>
+                            {:else}
+                                <div
+                                    class="username-block inline-block rounded-full py-1 px-3 text-xs font-extrabold"
+                                >
+                                    {username.username}
+                                </div>
+                            {/if}
+                        {/each}
+                    {/if}
                 </div>
             </div>
-        </div>
-        <div class="relative text-right">
-            <h1 class="text-md md:block">
-                <span class="">{$balance.toFixed(6)}</span>
-                <span class="opacity-50">SOL</span>
-            </h1>
+            <div class="relative text-right">
+                <h1 class="text-md md:block">
+                    <span class="">{$balance.toFixed(6)}</span>
+                    <span class="opacity-50">SOL</span>
+                </h1>
 
-            {#if !$price?.isLoading}
-                <span class="ml-1 text-xs opacity-50 md:block"
-                    >{formatMoney(worth)} USD</span
-                >
-            {/if}
-        </div>
-        <div class="relative w-1/4 px-3 text-right">
-            <h1 class="text-md md:block">
-                <span class="">{$balance.toFixed(6)}</span>
-                <span class="opacity-50">SOL</span>
-            </h1>
-
-            {#if !$price?.isLoading}
-                <span class="ml-1 text-xs opacity-50 md:block"
-                    >{formatMoney(worth)} USD</span
-                >
-            {/if}
+                {#if !$price?.isLoading}
+                    <span class="ml-1 text-xs opacity-50 md:block"
+                        >{formatMoney(worth)} USD</span
+                    >
+                {/if}
+            </div>
         </div>
     </div>
-</div>
+</Username>
--- apps/web/src/lib/trpc/routes/account-usernames.ts
+++ apps/web/src/lib/trpc/routes/account-usernames.ts
@@ -2,10 +2,12 @@ import { t } from "$lib/trpc/t";
 
 import { z } from "zod";
 
-import { getAllDomains, reverseLookup } from "@bonfida/spl-name-service";
-// import { TldParser } from "@onsol/tldparser";
+// import { getAllDomains, reverseLookup } from "@bonfida/spl-name-service";
+import { HELIUS_KEY } from "$env/static/private";
+import { connect } from "@helius-labs/xray";
+import { TldParser } from "@onsol/tldparser";
+import type { MainDomain } from "@onsol/tldparser/dist/types/state/main-domain";
 import { Connection, PublicKey } from "@solana/web3.js";
-import connect from "src/lib/util/solana/connect";
 
 interface Username {
     type: "bonfida" | "backpack" | "ans";
@@ -28,40 +30,58 @@ const getBackpackUsername = async (usernames: Username[], address = "") => {
 
 const getSolanaDomain = async (
     usernames: Username[],
-    address = "",
-    connection: Connection
+    address = ""
+    // connection: Connection
 ) => {
     const domainKey = new PublicKey(address);
-    const allDomainKeys = await getAllDomains(connection, domainKey);
-    const domains: string[] = [];
-    if (allDomainKeys) {
-        for (const key of allDomainKeys) {
-            const domainName = await reverseLookup(connection, key);
-            usernames.push({
-                type: "bonfida",
-                username: `${domainName}.sol`,
-            });
-        }
-    }
-    return domains;
+    // const allDomainKeys = await getAllDomains(connection, domainKey);
+    // const domains: string[] = [];
+    // if (allDomainKeys) {
+    //     for (const key of allDomainKeys) {
+    //         const domainName = await reverseLookup(connection, key);
+    //         username     s.push({
+    //             type: "bonfida",
+    //             username: `${domainName}.sol`,
+    //         });
+    //     }
+    // }
+    const response = await fetch(
+        `https://sns-sdk-proxy.bonfida.workers.dev/reverse-lookup/${address}`
+    );
+    const { result = "" } = await response.json();
+    console.log(result);
+    // return domains;
 };
 
-// const getANSDomain = async (address = "", connection: Connection) => {
-//     const ans = new TldParser(connection);
-//     let domain;
+const getANSDomain = async (address = "", connection: Connection) => {
+    // const RPC_URL =
+    //     "https://newest-intensive-choice.solana-mainnet.discover.quiknode.pro/b14717fce4a4f1e59e7287e5ac9bdf40fdada346/";
+    // connection = new Connection(RPC_URL);
+    console.log(new PublicKey("7hPhaUpydpvm8wtiS3k4LPZKUmivQRs7YQmpE1hFshHx"));
+    // const owner = new PublicKey("7hPhaUpydpvm8wtiS3k4LPZKUmivQRs7YQmpE1hFshHx");
+    // const owner = new PublicKey(address);
+    const ans = new TldParser(connection);
+    // const owner = await ans.getAllUserDomains(address);
+    // const owner = new PublicKey(address);
+    let domain: MainDomain;
+    // let domain = await ans.getMainDomain(address);
 
-//     try {
-//         domain = await ans.getMainDomain(address);
-//     } catch (error) {
-//         return "";
-//     }
+    try {
+        domain = await ans.getMainDomain(owner);
+        console.log("ji", domain);
+    } catch (error) {
+        console.log("a", error);
+        return "";
+    }
 
-//     if (domain && domain?.domain && domain?.tld) {
-//         return `${domain.domain}${domain.tld}`;
-//     }
+    if (domain && domain?.domain && domain?.tld) {
+        return `${domain.domain}${domain.tld}`;
+    }
 
-//     return "";
-// };
+    console.log("d", domain);
+
+    return "";
+};
 
 export const accountUsernames = t.procedure
     .input(z.string())
@@ -74,17 +94,17 @@ export const accountUsernames = t.procedure
         )
     )
     .query(async ({ input: address }) => {
-        const connection = connect();
+        const connection = connect("mainnet", HELIUS_KEY);
         const usernames: Username[] = [];
         await getBackpackUsername(usernames, address);
-        await getSolanaDomain(usernames, address, connection);
-        // const ansDomain = await getANSDomain(address, connection);
+        await getSolanaDomain(usernames, address);
+        const ansDomain = await getANSDomain(address, connection);
 
-        // if (ansDomain) {
-        //     usernames.push({
-        //         type: "ans",
-        //         username: ansDomain,
-        //     });
-        // }
+        if (ansDomain) {
+            usernames.push({
+                type: "ans",
+                username: ansDomain,
+            });
+        }
         return usernames || [];
     });
