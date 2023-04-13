9136852a7f9d3e381d52fffc3e17dda083eb50d7
ssamkkim <ssamuel.kkim@gmail.com>
2023-04-13 02:00:47 -0700

    nft history trpc route scaffold

---
+++ apps/web/src/lib/trpc/routes/nft-history.ts
@@ -0,0 +1,78 @@
+import type { EnrichedTransaction } from "helius-sdk";
+
+import { parseTransaction } from "@helius-labs/xray";
+
+import { t } from "$lib/trpc/t";
+
+import { z } from "zod";
+
+const { HELIUS_KEY } = process.env;
+
+export const transactions = t.procedure
+    .input(
+        z.object({
+            account: z.string(),
+            cursor: z.string().optional(),
+            filter: z.string().optional(),
+            user: z.string().optional(),
+        })
+    )
+    .output(
+        z.object({
+            oldest: z.string(),
+            result: z.array(
+                z.object({
+                    accounts: z.array(
+                        z.object({
+                            account: z.string(),
+                            changes: z.array(
+                                z.object({
+                                    amount: z.number(),
+                                    mint: z.string(),
+                                })
+                            ),
+                        })
+                    ),
+                    actions: z.array(
+                        z.object({
+                            actionType: z.string(),
+                            amount: z.number(),
+                            from: z.string(),
+                            fromName: z.string().optional(),
+                            received: z.string().optional(),
+                            sent: z.string().optional(),
+                            to: z.string(),
+                            toName: z.string().optional(),
+                        })
+                    ),
+                    fee: z.number(),
+                    primaryUser: z.string(),
+                    raw: z.any(),
+                    signature: z.string(),
+                    source: z.string(),
+                    timestamp: z.number(),
+                    type: z.string(),
+                })
+            ),
+        })
+    )
+    .query(async ({ input }) => {
+        const url = `https://api.helius.xyz/v1/nft-events?api-key=${HELIUS_KEY}`;
+
+        const response = await fetch(url, {
+            body: JSON.stringify({
+                query: {
+                    accounts: [input.account],
+                },
+            }),
+        });
+
+        const json: EnrichedTransaction[] = await response.json();
+
+        const result = json.map((tx) => parseTransaction(tx, input.user)) || [];
+
+        return {
+            oldest: json[json.length - 1]?.signature || "",
+            result,
+        };
+    });
