3933eda985ac88991fb62b504b816b383644b033
ssamkkim <ssamuel.kkim@gmail.com>
2023-04-13 18:42:34 -0700

    bonfida domain

--- apps/web/src/lib/trpc/routes/account-usernames.ts
+++ apps/web/src/lib/trpc/routes/account-usernames.ts
@@ -3,11 +3,12 @@ import { t } from "$lib/trpc/t";
 import { z } from "zod";
 
 // import { getAllDomains, reverseLookup } from "@bonfida/spl-name-service";
-import { HELIUS_KEY } from "$env/static/private";
 import { connect } from "@helius-labs/xray";
 import { TldParser } from "@onsol/tldparser";
 import type { MainDomain } from "@onsol/tldparser/dist/types/state/main-domain";
 import { Connection, PublicKey } from "@solana/web3.js";
+
+const { HELIUS_KEY } = process.env;
 
 interface Username {
     type: "bonfida" | "backpack" | "ans";
@@ -28,46 +29,36 @@ const getBackpackUsername = async (usernames: Username[], address = "") => {
     }
 };
 
-const getSolanaDomain = async (
-    usernames: Username[],
-    address = ""
-    // connection: Connection
-) => {
-    const domainKey = new PublicKey(address);
-    // const allDomainKeys = await getAllDomains(connection, domainKey);
-    // const domains: string[] = [];
-    // if (allDomainKeys) {
-    //     for (const key of allDomainKeys) {
-    //         const domainName = await reverseLookup(connection, key);
-    //         username     s.push({
-    //             type: "bonfida",
-    //             username: `${domainName}.sol`,
-    //         });
-    //     }
-    // }
-    const response = await fetch(
-        `https://sns-sdk-proxy.bonfida.workers.dev/reverse-lookup/${address}`
-    );
-    const { result = "" } = await response.json();
-    console.log(result);
-    // return domains;
+const getSolanaDomain = async (usernames: Username[], address = "") => {
+    const url = `https://api.helius.xyz/v0/addresses/${address}/names?api-key=${HELIUS_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data?.domainNames) {
+        for (const domain of data.domainNames) {
+            usernames.push({
+                type: "bonfida",
+                username: `${domain}.sol`,
+            });
+        }
+    }
 };
 
 const getANSDomain = async (address = "", connection: Connection) => {
     // const RPC_URL =
     //     "https://newest-intensive-choice.solana-mainnet.discover.quiknode.pro/b14717fce4a4f1e59e7287e5ac9bdf40fdada346/";
     // connection = new Connection(RPC_URL);
-    console.log(new PublicKey("7hPhaUpydpvm8wtiS3k4LPZKUmivQRs7YQmpE1hFshHx"));
+    // console.log(new PublicKey("7hPhaUpydpvm8wtiS3k4LPZKUmivQRs7YQmpE1hFshHx"));
     // const owner = new PublicKey("7hPhaUpydpvm8wtiS3k4LPZKUmivQRs7YQmpE1hFshHx");
     // const owner = new PublicKey(address);
     const ans = new TldParser(connection);
     // const owner = await ans.getAllUserDomains(address);
     // const owner = new PublicKey(address);
-    let domain: MainDomain;
+    let domain;
     // let domain = await ans.getMainDomain(address);
 
     try {
-        domain = await ans.getMainDomain(owner);
+        domain = await ans.getMainDomain(address);
         console.log("ji", domain);
     } catch (error) {
         console.log("a", error);
@@ -78,7 +69,7 @@ const getANSDomain = async (address = "", connection: Connection) => {
         return `${domain.domain}${domain.tld}`;
     }
 
-    console.log("d", domain);
+    // console.log("d", domain);
 
     return "";
 };
