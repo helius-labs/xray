d7e209ba8b7e02030f541c16ef27e183acc2d2f4
ssamkkim <ssamuel.kkim@gmail.com>
2023-04-24 14:10:32 -0700

    log messages component

--- apps/web/src/lib/components/log-messages.svelte
+++ apps/web/src/lib/components/log-messages.svelte
@@ -1,3 +1,11 @@
-<script>
+<script lang="ts">
     export let logs: string[];
 </script>
+
+{#each logs as message}
+    <p class="px-3 text-xs">
+        <span class="mr-1 text-success">></span><span class="text-neutral"
+            >{message}</span
+        >
+    </p>
+{/each}
