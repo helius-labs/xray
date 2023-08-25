4ea175af1b062dcd784f0e7b33a6f9e9ef7087dc
Qudo <qudo@matr.world>
2023-08-25 12:16:23 -0700

    asset gallery

--- src/routes/account/[account]/assets/+page.svelte
+++ src/routes/account/[account]/assets/+page.svelte
@@ -8,30 +8,35 @@
 
     const client = trpcWithQuery($page);
 
-    const createAssetsQuery = (input : {
-        account: string,
-        cursor: number
-    }) => client.assets.createInfiniteQuery(input, {
-        getNextPageParam: (lastPage) => lastPage.page + 1,
-        refetchOnMount: false,
-        refetchOnWindowFocus: false,
-    });
+    const createAssetsQuery = (input: { account: string; cursor: number }) =>
+        client.assets.createInfiniteQuery(input, {
+            getNextPageParam: (lastPage) => lastPage.page + 1,
+            refetchOnMount: false,
+            refetchOnWindowFocus: false,
+        });
 
     $: assets = createAssetsQuery({
         account,
         cursor: 1,
     });
 
-    $: lastPageHadAssets = $assets.data?.pages[$assets.data.pages.length - 1].total > 0;
+    $: lastPageHadAssets =
+        $assets.data?.pages[$assets.data.pages.length - 1].total > 0;
 </script>
 
-<div class="grid grid-cols-3 md:grid-cols-5 gap-3">
+<div class="grid grid-cols-3 gap-3 md:grid-cols-5">
     {#each $assets.data?.pages || [] as page}
         {#each page.items as asset}
-            {@const image = asset.content.files.find((file) => file.mime.startsWith("image") && file.uri)}
+            {@const image = asset.content.files.find(
+                (file) => file.mime.startsWith("image") && file.uri
+            )}
 
             <a href="/token/{asset.id}">
-                <Image src={image?.uri} className="aspect-square w-full rounded-lg" alt="" />
+                <Image
+                    src={image?.uri}
+                    className="aspect-square w-full rounded-lg"
+                    alt=""
+                />
             </a>
         {/each}
     {/each}
