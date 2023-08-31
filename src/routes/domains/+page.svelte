4ec7cb3b7a92447115ca8a2176460a04504b658d
hunter <104146303+solhosty@users.noreply.github.com>
2023-08-31 18:53:30 -0400

    search and domains created

---
+++ src/routes/domains/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-center p-3 text-2xl font-bold mt-5"> Search Bonfida Domains</h1>
+<div class="flex p-3 w-full justify-center"> 
+<input type="text" placeholder="Type here" class="input w-2/4 input-bordered my-2 max-w-xs" />
+</div>
