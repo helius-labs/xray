15217b4da3e0bb7f0875c90fb7414dffbaa197f6
0xIchigo <0xIchigo@protonmail.com>
2023-10-14 00:55:38 -0400

    Simplify getMimeType

--- src/lib/util/get-mime-type.ts
+++ src/lib/util/get-mime-type.ts
@@ -1,18 +1,6 @@
-const getMimeType = (filename: string): string | null => {
-    const parts = filename.split(".");
-    const extension = parts.length > 1 ? parts.pop()!.toLowerCase() : null;
-
-    if (extension === "mp4") {
-        return "video/mp4";
-    } else if (extension === "webm") {
-        return "video/webm";
-    } else if (extension === "ogg") {
-        return "video/ogg";
-    } else if (extension === "mov") {
-        return "video/quicktime";
-    } else {
-        return null;
-    }
+const getMimeType = async (url: string) => {
+    const response = await fetch(url, { method: "HEAD" });
+    return response.headers.get("Content-Type");
 };
 
 export default getMimeType;
