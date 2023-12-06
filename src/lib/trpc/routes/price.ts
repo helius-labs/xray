6ed698f82601d936f26eab01b0fd9bb95948881b
Joe Ellul-Turner <joeetwork@gmail.com>
2023-12-06 09:12:34 +0000

    removed birdeye api key exposure to the client side

--- src/lib/trpc/routes/price.ts
+++ src/lib/trpc/routes/price.ts
@@ -2,17 +2,16 @@ import { z } from "zod";
 
 import { t } from "$lib/trpc/t";
 
-import { VITE_BIRDEYE_API_KEY } from "$env/static/private";
-
 export const price = t.procedure
     .input(z.string())
     .query(async ({ input: token }) => {
+        const apiKey = import.meta.env.BIRDEYE_API_KEY;
         const response = await fetch(
             `https://public-api.birdeye.so/public/price/?address=${token}`,
             {
                 headers: {
                     Accept: "application/json",
-                    "X-API-KEY": VITE_BIRDEYE_API_KEY,
+                    "X-API-KEY": apiKey,
                     "x-chain": "solana",
                 },
             }
