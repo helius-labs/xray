a8c14c6650e7fb30c3982463e2117f7736408ab3
0xIchigo <0xIchigo@protonmail.com>
2024-01-08 20:35:17 -0500

    Allow Users to Copy the Anchor IDL

--- src/routes/account/[account]/idl/+page.svelte
+++ src/routes/account/[account]/idl/+page.svelte
@@ -2,10 +2,23 @@
     import { idlStore } from "$lib/util/stores/idl";
     // @ts-ignore
     import formatHighlight from "json-format-highlight";
+    import { copyToClipboard } from "$lib/util/clipboard";
+    import CopyButton from "$lib/components/copy-button.svelte";
 
     let formattedIdl = "";
+    let idlText = "";
+    let isCopying = false;
+
+    const handleCopyClick = () => {
+        if ($idlStore) {
+            copyToClipboard(idlText);
+            isCopying = true;
+            setTimeout(() => (isCopying = false), 500);
+        }
+    };
 
     $: if ($idlStore) {
+        idlText = JSON.stringify($idlStore, null, 2);
         formattedIdl = formatHighlight(JSON.stringify($idlStore, null, 2), {
             keyColor: "#a5a3a",
             numberColor: "#e8a034",
@@ -16,8 +29,16 @@
 
 <div class="relative mx-auto w-full max-w-2xl pb-32">
     {#if $idlStore}
-        <div class="code col-span-12 w-full overflow-hidden px-3">
-            <pre><code class="text-xs">{@html formattedIdl}</code></pre>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div class="flex flex-col justify-between items-center">
+            <div
+                class="code col-span-12 w-full rounded-lg border px-3 py-2 hover:border-primary cursor-pointer"
+                class:cursor-copy={isCopying}
+                on:click={handleCopyClick}
+            >
+                <pre><code class="text-xs">{@html formattedIdl}</code></pre>
+            </div>
+            <CopyButton text={idlText} classList="btn-sm mt-4" label="Copy IDL" success="Copied!" />
         </div>
     {:else}
         <p>No IDL data available.</p>
