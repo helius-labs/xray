96f4387678723687cbc4f226c62d7f8dd01a0691
0xIchigo <0xIchigo@protonmail.com>
2024-01-23 02:42:28 -0500

    Add Burnt Field to JSON Metadata

--- src/lib/components/providers/token-provider.svelte
+++ src/lib/components/providers/token-provider.svelte
@@ -92,6 +92,7 @@
         metadata.video_uri = data?.content.files?.find((file: any) =>
             file.mime?.startsWith("video/")
         )?.uri;
+        metadata.burnt = data?.burnt;
     }
 
     $: if (data?.id && !metadata.image) {
